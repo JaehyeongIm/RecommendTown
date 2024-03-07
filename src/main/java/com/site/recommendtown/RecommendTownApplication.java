0d82beb2380ecc49c5fefba2f9bd559e104e2c7a
imjaehyeong <dlawogud517@gmail.com>
2024-03-07 18:03:11 +0900

    Initial commit

---
+++ src/main/java/com/site/recommendtown/RecommendTownApplication.java
@@ -0,0 +1,13 @@
+package com.site.recommendtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendTownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/site/recommendtown/RecommendTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.recommendtown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
